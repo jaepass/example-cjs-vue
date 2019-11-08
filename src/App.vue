715e6a8ba0c4c53583498cf230ec740c49b5985b
Jaeriah <jaeriahpasseri@gmail.com>
2019-11-08 14:06:23 -0800

    output category list

--- src/App.vue
+++ src/App.vue
@@ -6,10 +6,9 @@
 
     <div class="container mx-auto px-4">
       <div class="flex mb-4">
-        <div class="row">
-          <p>{{ categories.name }}</p>
-          <!-- :key is for Vue to keep track of items with ids -->
-          <!-- <div class="col-sm-4" v-for="category in categories" :key="categories.id" > -->
+        <div class="row" v-for="category in categories" :key="category">
+          <p>{{ category.name }}</p>
+          <p>{{ category.description }}</p>
           </div>
         </div>
       </div>
@@ -19,18 +18,13 @@
 </template>
 
 
-
 <script>
-
-//Import Commerce.js products
 import Commerce from '@chec.io/commerce';
 import Hero from "@/components/Hero.vue";
 import Footer from "@/components/Footer.vue";
 
-
 // Initialize store with public key, store key in variable myStore
 const myStore = new Commerce('pk_17054571618e73520760e522b00e08ee196503b14e95c', true);
-
 
 export default {
   name: "app",
@@ -40,34 +34,16 @@ export default {
   },
   data() {
     return {
-      categories:[
-        {name: "Leather", description: "Classic leather timepieces"}
-      ]
+      categories:[]
     };
   },
 
-
-// //When element is mounted, look up data with store instance key
-//   mounted() {
-//     //List all products from chec 
-//     myStore.products.list()
-//       .then((resp) => {
-//         //console.log(resp);
-//         this.products = resp.data
-//         this.loading = false
-//       })
-//       .catch((error) => {
-//                 alert(error);
-//             });  
-//   },
-
-    mounted() {
+  mounted() {
     //List all products from chec 
-    myStore.products.list()
+    myStore.categories.list()
       .then((resp) => {
         //console.log(resp);
-        this.products = resp.data
-        this.loading = false
+        this.categories = resp.data
       })
       .catch((error) => {
                 alert(error);
@@ -79,16 +55,6 @@ export default {
     addToWishlist(product) {
       this.wishlist.push(product);
     },
-    isInWishlist(product) {
-      const item = this.wishlist.find(item => item.id === product.id);
-      if (item) {
-        return true;
-      }
-      return false;
-    },
-    removeFromWishlist(product) {
-      this.wishlist = this.wishlist.filter(item => item.id !== product.id);
-    } 
   }
 };
 </script>
@@ -106,6 +72,5 @@ h1, h2, h3, h4{
 p, a{
   letter-spacing: 2.5px;
 }
-
 
 </style>
